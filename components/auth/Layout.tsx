3057fa05745445599a72ace7c938dbf9876393a0
Kwaku Boohene <kwaku.kwayisi@gmail.com>
2022-02-27 21:38:35 +0000

    updated graphs and package.json

--- components/auth/Layout.tsx
+++ components/auth/Layout.tsx
@@ -1,10 +1,15 @@
 import styles from "../../styles/auth/auth.module.scss";
 import type {ReactElement} from 'react'
+import Head from "next/head";
 
 
 function Layout(children:ReactElement){
     return(
         <div className={styles.page_layout}>
+            <Head>
+                <title>SaveMe - A Personal Accounting App for your everyday internet user</title>
+                <link rel="icon" href="./assets/icons/icon-transparent.png" />
+            </Head>
         <div className="container">
             <div className="center">
             {children}
